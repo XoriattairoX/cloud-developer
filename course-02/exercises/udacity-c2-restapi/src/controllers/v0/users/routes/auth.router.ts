a1c04f111343615bc02c5931799050008d8875a7
Xoriat <i_registration@outlook.com>
2021-05-02 15:20:40 +0200

    authorization function uncommented and jwt implemented

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -1,12 +1,14 @@
 import { Router, Request, Response } from 'express';
 
 import { User } from '../models/User';
+import {config} from './../../../../config/config';
 
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import { NextFunction } from 'connect';
 
 import * as EmailValidator from 'email-validator';
+import { Config } from 'aws-sdk';
 
 const router: Router = Router();
 
@@ -28,30 +30,31 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
 
 function generateJWT(user: User): string {
     //@TODO Use jwt to create a new JWT Payload containing
-    return "NotYetImplemented"
+    return jwt.sign(user.toJSON(), config.jwt.secret);
+    //return "NotYetImplemented"
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
     console.warn("auth.router not yet implemented, you'll cover this in lesson 5")
     return next();
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
+     if (!req.headers || !req.headers.authorization){
+         return res.status(401).send({ message: 'No authorization headers.' });
+     }
     
 
-    // const token_bearer = req.headers.authorization.split(' ');
-    // if(token_bearer.length != 2){
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
+     const token_bearer = req.headers.authorization.split(' ');
+     if(token_bearer.length != 2){
+         return res.status(401).send({ message: 'Malformed token.' });
+     }
     
-    // const token = token_bearer[1];
+     const token = token_bearer[1];
 
-    // return jwt.verify(token, "hello", (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+     return jwt.verify(token, "hello", (err, decoded) => {
+       if (err) {
+         return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+       }
+       return next();
+     });
 }
 
 router.get('/verification', 
